5246f1ee00344cbb64948e72f947f18e9b2bb988
charlie.jackson <charlie.jackson>
2015-11-04 17:25:53 +0000

    Reordered share icons

--- sections/post-footer.php
+++ sections/post-footer.php
@@ -1,6 +1,6 @@
 <footer class="clearfix">
 	<span class="post-share-on-text">Share this post on:</span>
-	<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>" class="post-share-button post-share-button-facebook" target="_blank"><i class="fa fa-facebook"></i></a>
 	<a href="https://twitter.com/intent/tweet?url=<?php echo urlencode( get_permalink() ); ?>&text=<?php echo urlencode( 'text of some sort' ); ?>&via=Vikiibell" class="post-share-button post-share-button-twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+	<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>" class="post-share-button post-share-button-facebook" target="_blank"><i class="fa fa-facebook"></i></a>
 	<a href="mailto:?subject=<?php echo rawurlencode( 'Viki\'s awesome site' ); ?>&body=<?php echo rawurlencode( 'Check out Viki\'s post at: ' . get_permalink() ); ?>" class="post-share-button post-share-button-email"><i class="fa fa-envelope"></i></a>
 </footer>
