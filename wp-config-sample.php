9c60927b8d7bf05c377c4d9a96cec6e0bca660ce
root <root@ip-172-31-17-122.eu-west-1.compute.internal>
2016-05-13 13:03:26 +0000

    Updated wordpress core from live

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -19,23 +19,20 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'database_name_here');
+
 /** MySQL database username */
-define('DB_USER', 'xxx');
+define('DB_USER', 'username_here');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'xxx');
+define('DB_PASSWORD', 'password_here');
 
 /** MySQL hostname */
-define('DB_HOST', 'xxx');
-
-if('admin.vikibell.com' === $_SERVER['HTTP_HOST']) {
-    define('DB_NAME', 'xxx');
-} else {
-    define('DB_NAME', 'xxx');
-}
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'xxx');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
@@ -49,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'xxx');
-define('SECURE_AUTH_KEY',  'xxx');
-define('LOGGED_IN_KEY',    'xxx');
-define('NONCE_KEY',        'xxx');
-define('AUTH_SALT',        'xxx');
-define('SECURE_AUTH_SALT', 'xxx');
-define('LOGGED_IN_SALT',   'xxx');
-define('NONCE_SALT',       'xxx');
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
 /**#@-*/
 
@@ -86,7 +83,7 @@ define('WP_DEBUG', false);
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-    define('ABSPATH', dirname(__FILE__) . '/');
+	define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
