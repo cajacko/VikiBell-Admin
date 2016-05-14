eabffab4872ba03e9e40b0bbb6cfe45e3c4a3c6e
cajacko <charlie@Charless-MacBook-Pro.local>
2016-05-14 09:17:05 +0100

    Added functions to update sitemap and purge cloud flare cache

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -18,6 +18,10 @@
  * @package WordPress
  */
 
+define('CLOUDFLARE_EMAIL', 'xxx');
+define('CLOUDFLARE_API', 'xxx');
+define('CLOUDFLARE_ZONE', 'xxx');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'database_name_here');
--- wp-content/themes/admin/functions.php
+++ wp-content/themes/admin/functions.php
@@ -22,3 +22,61 @@ add_action( 'after_setup_theme', 'admin_setup' );
 @ini_set( ‘upload_max_size’ , ‘10G’);
 @ini_set( ‘post_max_size’, ‘10G’);  
 @ini_set( ‘max_execution_time’, ‘300’);
+
+// Update the sitemap on vikibell.com
+function update_sitemap() {
+    // Action url that builds the sitemap
+    $sitemap_action_url = 'https://vikibell.com/action/build-sitemap'; 
+
+    // Unique query string to bypass cache (needed due to limit in number of cloudflare rules)
+    $query_string = '?v=' . time();
+    $sitemap_action_url .= $query_string;
+
+    // Trigger the action and get the response
+    $response = file_get_contents($sitemap_action_url);
+    $reponse = json_decode($response);
+
+    // If the action was successful then return true
+    if(isset($response['reponse']) && $response['reponse']) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// update_sitemap();
+
+function sync_media() {
+
+}
+
+// Due to free cloudflare plan we must purge everything
+function clear_page_cache() {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.cloudflare.com/client/v4/zones/" . CLOUDFLARE_ZONE . "/purge_cache");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+
+    $headers = [ 
+        'X-Auth-Email: ' . CLOUDFLARE_EMAIL,
+        'X-Auth-Key: ' . CLOUDFLARE_API,
+        'Content-Type: application/json'
+    ];
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $dnsData = array('purge_everything' => true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($dnsData));
+
+    $result = json_decode(curl_exec($ch), true);
+
+    if(isset($result['success']) && $result['success']) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// clear_page_cache();
+// exit;
+
